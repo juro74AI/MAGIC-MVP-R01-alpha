3f767869473daff5c2504d09d937252ec8fc7e7d
juro74AI <juro74@protonmail.com>
2025-06-22 09:27:31 +0200

    Configure HTTPS with @vitejs/plugin-basic-ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl()
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     host: '0.0.0.0',
     port: 5173,
+    https: true,
   },
   preview: {
     host: '0.0.0.0',
     port: 4173,
+    https: true,
   }
 });
