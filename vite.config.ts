44a176ff90739ea667885278a905aa8ac84c4b72
juro74AI <juro74@protonmail.com>
2025-06-22 09:28:58 +0200

    Fix SSL Plugin Error

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    basicSsl()
+    react()
   ],
   optimizeDeps: {
     exclude: ['lucide-react'],
@@ -14,11 +12,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    https: true,
   },
   preview: {
     host: '0.0.0.0',
     port: 4173,
-    https: true,
   }
 });
